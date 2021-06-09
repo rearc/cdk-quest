6a2556fb648c96cb763b1f580aeae9294bcba4e6
Nicholas Hofbauer <nicholas.hofbauer@rearc.io>
2021-06-09 16:09:10 -0400

    Updated glue consumer script

--- data-pipeline/src/glue/consumer.py
+++ data-pipeline/src/glue/consumer.py
@@ -1,26 +1,43 @@
 import boto3
 
+s3 = boto3.resource('s3')
 sqs = boto3.client('sqs')
 
-queue_url = 'SQS_QUEUE_URL' # Todo: Parameterize this
+queue_url = 'https://sqs.us-east-1.amazonaws.com/209490147253/GsQuestDataPipelineStack-gsquestdatapipelinequeue24B34FEE-F5TF0P2LKRRR.fifo' # Todo: Parameterize this
 
 # Receive message from SQS queue
 response = sqs.receive_message(
   QueueUrl=queue_url,
   AttributeNames=[
-    'SentTimestamp'
+    'vendorName',
+    'fileName'
   ],
   MaxNumberOfMessages=1, # Should we receive more messages?
   MessageAttributeNames=[
     'All'
   ],
-  VisibilityTimeout=0,
+  VisibilityTimeout=5,
   WaitTimeSeconds=0
 )
 
 message = response['Messages'][0]
-# Todo: Parse message which contains file location in the vendor s3 bucket then copy this file to the data bucket
+print('Successfully retrieved file: %s' % message)
+print('Message Body: %s' % message['Body'])
+print('Message Attributes: %s' % message['MessageAttributes'])
 
+vendorName = message['MessageAttributes']['vendorName']['StringValue']
+print('VendorName: %s' % vendorName)
+
+fileName = message['MessageAttributes']['fileName']['StringValue']
+print('FileName: %s' % fileName)
+
+copy_source = {
+  'Bucket': 'gs-quest-data-pipeline-vendor-bucket', # Todo: Parameterize this
+  'Key': vendorName + '/' + fileName
+}
+bucket = s3.Bucket('gs-quest-data-pipeline-data-bucket') # Todo: Parameterize this
+bucket.copy(copy_source, vendorName + '-' + fileName)
+print('Successfully copied message from the vendor bucket to the data bucket')
 
 receipt_handle = message['ReceiptHandle']
 
@@ -28,4 +45,4 @@ sqs.delete_message(
   QueueUrl=queue_url,
   ReceiptHandle=receipt_handle
 )
-print('Successfully processed message: %s' % message)
+print('Successfully deleted message')
--- scripts/uploadFile.sh
+++ scripts/uploadFile.sh
@@ -6,14 +6,16 @@ vendorName=$1
 vendorBucket=$2
 queueUrl=$3
 
-file=$(uuidgen).txt
-echo "Creating temp file: $file"
+fileName=$(uuidgen).txt
+echo "Creating temp fileName: $fileName"
 
-openssl rand -base64 200 -out $file
+openssl rand -base64 200 -out $fileName
 
-echo "Uploading file to s3://$vendorBucket/$vendorName/$file"
-aws s3 cp $file s3://$vendorBucket/$vendorName/$file
+echo "Uploading file to s3://$vendorBucket/$vendorName/$fileName"
+aws s3 cp $fileName s3://$vendorBucket/$vendorName/$fileName
 
-rm $file
+rm $fileName
 
-aws sqs send-message --queue-url $queueUrl --message-body "s3://$vendorBucket/$vendorName/$file" --message-group-id $vendorName
+messageAttributes=$(jq -c -n --arg vendorName "$vendorName" --arg fileName "$fileName" '{vendorName: {DataType:"String", StringValue:$vendorName}, fileName: {DataType:"String", StringValue:$fileName}}')
+
+aws sqs send-message --queue-url $queueUrl --message-body "S3 Vendor File: s3://$vendorBucket/$vendorName/$fileName" --message-group-id $vendorName --message-attributes $messageAttributes
